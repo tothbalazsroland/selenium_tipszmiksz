9b377b53289701a28ae901a8e5b69342b642caf3
Clethcriss <balazs.borsi.1986@gmail.com>
2019-02-28 15:44:18 +0100

    Select 'Labdarugas' collect data from 'table-default' and only nums, title and market classes

--- app.py
+++ app.py
@@ -6,5 +6,14 @@ import os
 
 
 browser = webdriver.Firefox()
-browser.get('http://seleniumhq.org/')
-print "Iam trying not to fuck up this one"
+browser.get('http://www.tippmix.hu/sportfogadas#?sportid=1')
+print("Hope it works")
+elements = browser.find_element_by_class_name('table-default').find_element_by_tag_name('tbody').find_elements_by_tag_name('tr')
+elements.pop()
+for element in elements:
+
+    print(element.find_element_by_class_name('nums').get_attribute('innerHTML'))
+    print(element.find_element_by_class_name('title').get_attribute('innerHTML'))
+    print(element.find_element_by_class_name('market').get_attribute('innerHTML'))
+
+browser.close()
